2289a8fbdcaaa650436baa587804dd4b1e08f79b
jakenogueira <jakeline.ezfront@mutant.com.br>
2023-08-07 17:12:07 -0300

    ajustes da estrutura

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var splide = new Splide('.splide-depositions', {
+    perMove: 3,
+    autoplay: false,
+    interval: 4000,
+    arrows: true,
+  } );
+
+splide.mount();
+
+var splide = new Splide('.splide-detailed-depositions', {
+  perMove: 1,
+  autoplay: false,
+  interval: 4000,
+  arrows: true,
+} );
+
+splide.mount();
